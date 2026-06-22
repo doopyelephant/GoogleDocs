10175d9acba1359ee9e5bf06fd53a958634485e4
doopyelephant <nolanqsp@gmail.com>
2026-06-22 07:15:55 -0400

    Set `$ProgressPreference` to `SilentlyContinue` and fix installer file extension in `Download&Install.ps1`.

--- Download&Install.ps1
+++ Download&Install.ps1
@@ -1,6 +1,7 @@
+$ProgressPreference = 'SilentlyContinue'
 $AppData = "$env:USERPROFILE\AppData\Local\GoogleDocs"
 $Release = "$AppData\InstallerRelease.zip"
 Invoke-WebRequest -Uri "https://github.com/doopyelephant/GoogleDocs/releases/download/v0.1.0-alpha/GoogleDocsInstaller-windows-x64.zip" -OutFile "$Release"
 Expand-Archive -Path $Release -DestinationPath "$AppData\InstallerRelease"
 Remove-Item -Force $Release
-& "$AppData\InstallerRelease\GoogleDocsInstaller"
+& "$AppData\InstallerRelease\GoogleDocsInstaller.exe"
