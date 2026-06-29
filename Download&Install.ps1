8291f4d203fbe24be8cdda7b7e458b183dc5f4ae
DoopyElephant <91019070+doopyelephant@users.noreply.github.com>
2026-06-29 07:59:42 -0400

    Create directories for Google Docs installation

--- Download&Install.ps1
+++ Download&Install.ps1
@@ -1,5 +1,6 @@
 $ProgressPreference = 'SilentlyContinue'
 $AppData = "$env:USERPROFILE\AppData\Local\GoogleDocs"
+mkdir $AppData
 $Release = "$AppData\InstallerRelease.zip"
 Invoke-WebRequest -Uri "https://github.com/doopyelephant/GoogleDocs/releases/download/v0.2.0-alpha/GoogleDocsInstaller-windows-x64.zip" -OutFile "$Release"
 mkdir "$AppData\InstallerRelease"
